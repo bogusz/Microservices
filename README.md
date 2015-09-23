9797016351a7371785206f9e5d3264328bb2b60a
Bogusz Pękalski <bpekalski@gmail.com>
2015-09-23 23:15:00 +0200

    First simple WebApi service and html page with rest call

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Microservices
+This project will be a web app using multiple rest microservices in diffrent languages.
+As a start there'll be ASP.NET WebApi and F# + Suave.
+
+Would you like to contribute? Be my guest :)
+
---
+++ src/webapi/WebApiService/Controllers/SayHelloController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApiService.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class SayHelloController : ApiController
+    {
+        // GET: api/service        
+        public IEnumerable<string> Get()
+        {
+            return new List<string>() { "Hello from WebApi service!" };
+        }
+
+        // GET: api/service/5
+        public string Get(int id)
+        {
+            return "Hello from WebApi service with id: " + id;
+        }
+    }
+}
